e57a299cc5bf298a1bc5803fec169487a621bc3b
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-08-09 01:55:27 -0400

    Update site.js
    Outlined js functions for Fibonacci algorithm

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Calls Hello World
-function HelloWorld() {
-    alert("Hello Worls");
+function getstartvalues(){
+
 }
+
+function generateFibonacci(){
+
+}
+/* function fibonacci(num){
+    var a = 1, b = 0, temp;
+
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  } */
+
+function generateFibonacciRecursive(){
+    
+}
+
+
+/* function fibonacci(num, memo) {
+    memo = memo || {};
+  
+    if (memo[num]) return memo[num];
+    if (num <= 1) return 1;
+  
+    return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+  } */
+  
+ function displayValues(){
+
+ }
